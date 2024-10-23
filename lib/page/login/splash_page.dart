96f2ac95fad1ee3ea101fda8a700b890b84ce15d
czeras <custom_mail@163.com>
2024-10-23 18:50:21 +0800

    fix：iphonex splash页底部按钮适配

--- lib/page/login/splash_page.dart
+++ lib/page/login/splash_page.dart
@@ -12,7 +12,6 @@ import '../../language/strings.dart';
 import '../../widget/common_btn.dart';
 
 class SplashPage extends BaseGetBuilder<UserController> {
-
   static const String routeName = '/';
 
   @override
@@ -23,32 +22,53 @@ class SplashPage extends BaseGetBuilder<UserController> {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset(Utils.getImgPath('bg_launch',dir: 'bg'),width: double.infinity,height: double.infinity,fit: BoxFit.cover,),
-          if(controller.isInited && controller.user == null)
-            _buildLoginBtn()
+          Image.asset(
+            Utils.getImgPath('bg_launch', dir: 'bg'),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          if (controller.isInited && controller.user == null) _buildLoginBtn()
         ],
       ),
     );
   }
 
-  _buildLoginBtn(){
-    return Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          margin: EdgeInsets.only(bottom: 50.w,left: 40.w,right: 40.w),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CommonBtn(onTap: () {
-                NavigatorUtils.toNamed(LoginPhoneCheckPage.routeName);
-              }, text: Ids.login.str(),textStyle: TextStyle(color: Colours.white,fontSize: 28.sp),backgroundColor: Colours.c_00CE3E,borderRadius:12.w , height: 80.w, width: 200.w ,),
-              CommonBtn(onTap: () {
-                  NavigatorUtils.toNamed(RegisterPage.routeName);
-              }, text: Ids.register.str(),textStyle: TextStyle(color: Colours.c_00CE3E,fontSize: 28.sp),backgroundColor: Colours.white,borderRadius:12.w, height: 80.w, width: 200.w ,
-              ),
-            ],
-          ),
-        ));
+  _buildLoginBtn() {
+    return SafeArea(
+      child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            margin: EdgeInsets.only(bottom: 50.w, left: 40.w, right: 40.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CommonBtn(
+                  onTap: () {
+                    NavigatorUtils.toNamed(LoginPhoneCheckPage.routeName);
+                  },
+                  text: Ids.login.str(),
+                  textStyle: TextStyle(color: Colours.white, fontSize: 28.sp),
+                  backgroundColor: Colours.c_00CE3E,
+                  borderRadius: 12.w,
+                  height: 80.w,
+                  width: 200.w,
+                ),
+                CommonBtn(
+                  onTap: () {
+                    NavigatorUtils.toNamed(RegisterPage.routeName);
+                  },
+                  text: Ids.register.str(),
+                  textStyle:
+                      TextStyle(color: Colours.c_00CE3E, fontSize: 28.sp),
+                  backgroundColor: Colours.white,
+                  borderRadius: 12.w,
+                  height: 80.w,
+                  width: 200.w,
+                ),
+              ],
+            ),
+          )),
+    );
   }
-
 }
